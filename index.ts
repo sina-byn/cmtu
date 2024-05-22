e71c390144aab7224ff97564498edea49f9d8903
sina-byn <sina.sina.bayan@protonmail.com>
2024-05-23 00:08:22 +0330

    cmtu core functionality added

--- index.ts
+++ index.ts
@@ -1,1 +1,51 @@
-// boilerplate code
+// * types
+type Resolver =
+  | string
+  | { block: string }
+  | { inline: string }
+  | { block: string; inline: string };
+
+const cmtu = (resolver: Resolver) => {
+  let pattern =
+    typeof resolver === 'string'
+      ? resolver
+      : Object.values(resolver).filter(Boolean).join('|');
+
+  const commentRegex = new RegExp(pattern, 'gm');
+
+  const exec = (code: string, replace: boolean = true): [string, string[]] => {
+    const matches: string[] = [];
+
+    code = code.replace(commentRegex, match => {
+      matches.push(match);
+      return replace ? '' : match;
+    });
+
+    return [code, matches];
+  };
+
+  return {
+    strip: (code: string) => exec(code, true)[0],
+    extract: (code: string) => exec(code, false)[1],
+    magic: (code: string) => exec(code, true),
+  };
+};
+
+module.exports = cmtu;
+
+const jsCode = `
+// this is a js comment
+
+this is not a js comment
+
+/*
+this is a js multi-line comment
+*/
+`;
+
+const jsCmtu = cmtu({ inline: '//.*', block: '/\\*(?:.|\n)*?\\*/' });
+
+const noComments = jsCmtu.strip(jsCode);
+
+console.log(noComments);
+
