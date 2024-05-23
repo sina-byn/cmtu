fb4f06632930e97b87b3128bf963cd91ed99d3e4
sina-byn <sina.sina.bayan@protonmail.com>
2024-05-23 11:38:19 +0330

    languages configs updated

--- languages.ts
+++ languages.ts
@@ -1,9 +1,9 @@
 // * types
 type Languages = Record<LanguageName, Language>;
 
-type LanguageName = 'JS';
-
 type Language = { resolver: Resolver; stringLiterals?: string[] };
+
+type LanguageName = 'JS' | 'CSS' | 'HTML' | 'CPP' | 'GO' | 'PYTHON' | 'PHP';
 
 export type Resolver =
   | string
@@ -11,10 +11,37 @@ export type Resolver =
   | { inline: string }
   | { block: string; inline: string };
 
+const DEFAULT_RESOLVER = { inline: '//.*', block: '/\\*(?:.|\n)*?\\*/' };
+const DEFAULT_STRING_LITERALS = ["'", '"'];
+
 const languages: Languages = {
   JS: {
-    resolver: { inline: '//.*', block: '/\\*(?:.|\n)*?\\*/' },
+    resolver: DEFAULT_RESOLVER,
     stringLiterals: ["'", '"', '`'],
+  },
+  CSS: {
+    resolver: DEFAULT_RESOLVER.block,
+    stringLiterals: DEFAULT_STRING_LITERALS,
+  },
+  HTML: {
+    resolver: '<!--[\\s\\S]*?-->',
+    stringLiterals: DEFAULT_STRING_LITERALS,
+  },
+  CPP: {
+    resolver: DEFAULT_RESOLVER,
+    stringLiterals: ['"'],
+  },
+  GO: {
+    resolver: DEFAULT_RESOLVER,
+    stringLiterals: ['"'],
+  },
+  PYTHON: {
+    resolver: '#.*',
+    stringLiterals: ["'", '"', "'''", '"""'],
+  },
+  PHP: {
+    resolver: { inline: '(?:#|//).*' },
+    stringLiterals: DEFAULT_STRING_LITERALS,
   },
 };
 
